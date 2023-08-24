5a6e1637117c87c3f821bfd928c01302545635a5
climacot <fermt111@gmail.com>
2023-08-23 19:56:35 -0500

    [FIX] next public env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { initializeApp, getApp, getApps } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_API_KEY,
-  appId: process.env.NEXT_APP_ID,
-  authDomain: process.env.NEXT_AUTH_DOMAIN,
-  measurementId: process.env.NEXT_MEASUREMENT_ID,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  projectId: process.env.NEXT_PROJECT_ID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 }
 
 const app = getApps().length <= 0 ? initializeApp(firebaseConfig) : getApp()
